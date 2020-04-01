1a2b0effebf79a21556a52091b802d03d6929c0d
FPC <izgifth@gmail.com>
2020-04-01 13:11:38 -0400

    Initial commit

---
+++ venv/app.py
@@ -0,0 +1,46 @@
+""" A Python program that prompts the user for
+    the name of a file with an arbitrary ASCII document,
+    reads the file, and prints a summary of the words in the document.
+
+    author: fizgi
+    date: 01-Apr-2020
+    python: v3.8.2
+"""
+
+from typing import IO, DefaultDict
+from collections import defaultdict
+from string import punctuation
+from operator import itemgetter
+
+
+file_name: str = input("Enter the file name: ")
+
+try:  # to open the file
+    path: IO = open(file_name, mode="r", encoding="utf-8")
+except FileNotFoundError:
+    print(f"File {file_name} is not found")
+else:
+    with path:  # close path after opening
+        total_words = 0
+        character_freq: DefaultDict[str, int] = defaultdict(int)
+        distinct_words: DefaultDict[str, int] = defaultdict(int)
+        punc_translator = str.maketrans({key: None for key in punctuation})
+
+        for line in path:
+            if line == "\n":
+                continue
+            clean_string = line.strip("\n").lower().translate(punc_translator)
+
+            for word in clean_string.split(" "):
+                total_words += 1
+                distinct_words[word] += 1
+                for char in word:
+                    if char.isalnum():
+                        character_freq[char] += 1
+
+        sorted_distinct_words = sorted(distinct_words.items(), key=itemgetter(1), reverse=True)
+        sorted_character_freq = sorted(character_freq.items(), key=itemgetter(1), reverse=True)
+
+    print(f"Total words: {total_words}\nTotal distinct words: {len(sorted_distinct_words)}")
+    print("Top 25 most frequent words:", [(word, '{:,}'.format(count)) for (word, count) in sorted_distinct_words[:25]])
+    print("Character frequency:", [(char, '{:,}'.format(count)) for (char, count) in sorted_character_freq])
